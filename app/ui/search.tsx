6050ed60b99f512769dbbdefc366a2aad10cc2fb
roberto <molinaroberto.cgp@gmail.com>
2024-02-13 10:00:46 -0300

    busqueda y paginacion terminada

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,28 +1,39 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  function handleSearch(term: string) {
+  //function handleSearch(term: string) {
+
+    const handleSearch = useDebouncedCallback((term) => {
+
     console.log(`Searching... ${term}`);
     
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+    
+  }, 300);
+
+//}
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
-        Search
+        Buscador - Search
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
